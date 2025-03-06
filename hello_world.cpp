1ac1284fa08b813a05726fff3429e517f65658a4
anhthoang <htnanh@gmail.com>
2025-03-05 22:28:25 -0800

    Add comment-HelloWorld Assignment

--- hello_world.cpp
+++ hello_world.cpp
@@ -5,6 +5,7 @@ using namespace std;
 /*
  * This is a simple Hello World program in C++.
  */
+// Hello World Assignment
 int main() {
     // Print Hello World to the terminal
     cout << "Hello World!" << endl;
