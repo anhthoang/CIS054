ef8f716abab18a9d6e9c28060822415cdec4e25e
Anh Hoang <anh.t.hoang@sjsu.edu>
2025-04-19 18:55:14 -0700

    add recursive function

--- extra-credit/extra_credit.cpp
+++ extra-credit/extra_credit.cpp
@@ -2,7 +2,14 @@
 using namespace std;
 
 //function to calculate the power of a number using recursion
-double power (double x,double n)
+double power (double x,double n){
+    //base case: if n = 0, x^n = 1
+    if(n == 0) return 1;
+
+    //Recursive case: x^n = x * x^(n-1)
+    return x * power(x, n - 1);
+
+}
 
 //main function tto run the program
 int main(){
