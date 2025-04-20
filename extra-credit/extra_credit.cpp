d3d25804c2f896d349b2a7490ceedee18ab7e6a0
Anh Hoang <anh.t.hoang@sjsu.edu>
2025-04-19 21:48:06 -0700

    clean up code

--- extra-credit/extra_credit.cpp
+++ extra-credit/extra_credit.cpp
@@ -1,35 +1,38 @@
 #include <iostream>
 using namespace std;
 
-//function to calculate the power of a number using recursion
-double power (double x,double n){
-    //base case: if n = 0, x^n = 1
-    if(n == 0) return 1;
+// function to calculate the power of a number using recursion
+double power(double x, double n)
+{
+    // base case: if n = 0, x^n = 1
+    if (n == 0)
+        return 1;
 
-    //Recursive case: x^n = x * x^(n-1)
+    // Recursive case: x^n = x * x^(n-1)
     return x * power(x, n - 1);
-
 }
 
-//main function to run the program
-int main(){
+// main function to run the program
+int main()
+{
 
-    //Declare value of x, n
+    // Declare value of x, n
     double x;
     int n;
 
-    //reading base number and exponent from user
+    // reading base number and exponent from user
     cout << "Enter a floating point number (x) " << endl;
     cin >> x;
     cout << "Enter a non-negative whole number (n) " << endl;
     cin >> n;
 
-    //Check if n is non-negative and even number
-    while (n < 0){
-        cout << "Please enter a non-negative whole number" << endl;
+    // Check if n is non-negative number
+    while (n < 0)
+    {
+        cout << "Please enter a non-negative number" << endl;
         cin >> n;
     }
-    double result = power(x,n);
+    double result = power(x, n);
     cout << x << " raised to the power of " << n << " is " << result << endl;
     return 0;
 }
