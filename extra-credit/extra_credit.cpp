0ee04a73c62d3a5be281474272df3bdac06b13c9
Anh Hoang <anh.t.hoang@sjsu.edu>
2025-04-19 18:52:17 -0700

    initial commit

---
+++ extra-credit/extra_credit.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+//function to calculate the power of a number using recursion
+double power (double x,double n)
+
+//main function tto run the program
+int main(){
+    return 0;
+}
